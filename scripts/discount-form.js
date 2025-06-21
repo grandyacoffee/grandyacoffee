05f4efccf0a3661a9103f7b86b8e442ba464c5fc
grandyacoffee <info.grandyacoffee@gmail.com>
2025-06-21 16:10:31 +0700

    Update discount-form.js

--- scripts/discount-form.js
+++ scripts/discount-form.js
@@ -43,21 +43,23 @@ function showDiscountForm() {
     modal.innerHTML = `
       <div class="discount-modal-overlay"></div>
       <div class="discount-modal-content">
-        <button class="discount-modal-close" aria-label="Close">&times;</button>
-        <h3>How to Get Your Discount</h3>
-        <ol style="margin-bottom:1.2em;">
-          <li>
-            Follow us on <a href="https://www.instagram.com/grandyacoffee/" target="_blank" rel="noopener" aria-label="Instagram"><img src="/icons/instagram.svg" alt="Instagram" class="social-icon"></a> or <a href="https://web.facebook.com/profile.php?id=61564243991797" target="_blank" rel="noopener" aria-label="Facebook"><img src="/icons/facebook.svg" alt="Facebook" class="social-icon"></a>
-          </li>
-          <li style="margin-top:0.8em;">
-            Message us on <a href="https://t.me/+85510759985" target="_blank" rel="noopener" aria-label="Telegram"><img src="/icons/telegram.svg" alt="Telegram" class="social-icon"></a><br>
-            <span style="font-size:0.98em;">Say: <b>Hello, I'm &lt;Your Name&gt;</b></span>
-          </li>
-          <li style="margin-top:0.8em;">
-            Or, Sign Up the Form Below
-          </li>
-        </ol>
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSf0spaeIY3qDV0ktow0Sa_40LNFK6MAFuuW7EaND92pReuDBQ/viewform?usp=header" target="_blank" rel="noopener" class="discount-submit-btn" style="display:block;text-align:center;">Sign Up to Get Discount!</a>
+      <button class="discount-modal-close" aria-label="Close">&times;</button>
+      <h3>How to Get Your Discount</h3>
+      <ol style="margin-bottom:1.2em;">
+        <li>
+        Follow us on <a href="https://www.instagram.com/grandyacoffee/" target="_blank" rel="noopener" aria-label="Instagram"><img src="/icons/instagram.svg" alt="Instagram" class="social-icon"></a> or <a href="https://web.facebook.com/profile.php?id=61564243991797" target="_blank" rel="noopener" aria-label="Facebook"><img src="/icons/facebook.svg" alt="Facebook" class="social-icon"></a>
+        </li>
+        <li style="margin-top:0.8em;">
+        Get in Touch on Telegram or WhatsApp
+        <a href="https://t.me/+85510759985" target="_blank" rel="noopener" aria-label="Telegram"><img src="/icons/telegram.svg" alt="Telegram" class="social-icon"></a> or
+        <a href="https://wa.me/85510759985" target="_blank" rel="noopener" aria-label="WhatsApp"><img src="/icons/whatsapp.svg" alt="WhatsApp" class="social-icon"></a><br>
+        <span style="font-size:0.98em;">Say: <b>Hello, I'm &lt;Your Name&gt;</b></span>
+        </li>
+        <li style="margin-top:0.8em;">
+        Or, Sign Up the Form Below
+        </li>
+      </ol>
+      <a href="https://docs.google.com/forms/d/e/1FAIpQLSf0spaeIY3qDV0ktow0Sa_40LNFK6MAFuuW7EaND92pReuDBQ/viewform?usp=header" target="_blank" rel="noopener" class="discount-submit-btn" style="display:block;text-align:center;">Sign Up to Get Discount!</a>
       </div>
     `;
     document.body.appendChild(modal);
